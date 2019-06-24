f882604c3d4eae816a29a86aa43e92a11b68cef6
David Georges <davidgeorges@MacBook-Pro-de-David-2.local>
2019-06-24 17:53:15 +0200

    Add index.php files for security

--- override/classes/index.php
+++ override/classes/index.php
@@ -23,17 +23,12 @@
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
-class Address extends AddressCore
-{
-    /**
-     * Address constructor.
-     * @param null $id_address
-     * @param null $id_lang
-     */
-    public function __construct($id_address = null, $id_lang = null)
-    {
-        self::$definition['fields']['vat_number'] = array('type' => self::TYPE_STRING, 'validate' => 'isVat');
-        parent::__construct($id_address, $id_lang);
-    }
-}
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
